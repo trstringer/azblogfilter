d52c308427d6040bdab8c31ada60a2f959b6c71f
Thomas Stringer <git@trstringer.com>
2020-09-01 19:48:20 -0400

    add config file back

--- cmd/root.go
+++ cmd/root.go
@@ -22,6 +22,7 @@ import (
 	"os"
 	"time"
 
+	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
 
 	"github.com/trstringer/azblogfilter/internal/cache"
@@ -121,6 +122,8 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.azblogfilter.yaml)")
+
 	rootCmd.Flags().StringVarP(&keywordsFilter, "keywords", "k", "", "keywords filter (case insensitive)")
 	rootCmd.Flags().StringVarP(&categoriesFilter, "categories", "c", "", "categories filter (case insensitive)")
 	rootCmd.Flags().StringVarP(&sinceFilter, "since", "s", "", "filter post with systemd time (man 7 systemd.time)")
@@ -131,6 +134,22 @@ func init() {
 }
 
 func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".azblogfilter" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".azblogfilter")
+	}
+
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
