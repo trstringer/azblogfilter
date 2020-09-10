02f04cf8b18035be8317eb1eb74a0ffed014d017
Thomas Stringer <git@trstringer.com>
2020-09-09 22:06:58 -0400

    change notification timeout to forever

--- scripts/notify.sh
+++ scripts/notify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cat - |
-    awk -F ',' '{printf("dunstify \"Azure\" '"'"'<a href=\"%s\">%s</a>'"'"'\n"), $3, $2}' |
+    awk -F ',' '{printf("dunstify --timeout 0 \"Azure\" '"'"'<a href=\"%s\">%s</a>'"'"'\n"), $3, $2}' |
     bash
